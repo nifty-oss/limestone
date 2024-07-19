04c93eb5c20c5a4919c1873e25335ad7b111a801
febo <febo@nifty-oss.org>
2024-07-19 01:16:30 +0100

    Update kinobi version

--- clients/rust/src/pda.rs
+++ clients/rust/src/pda.rs
@@ -1,5 +1,6 @@
 use solana_program::pubkey::Pubkey;
 
+/// Return the PDA and bump seed for the given `base` and `slot`.
 pub fn find_pda(base: &Pubkey, slot: u64) -> (Pubkey, u8) {
     Pubkey::find_program_address(&[base.as_ref(), &slot.to_le_bytes()], &crate::ID)
 }
