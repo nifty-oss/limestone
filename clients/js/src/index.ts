a9fdd7a6f366e9074c76e808c1b850aa70289335
febo <febo@nifty-oss.org>
2024-07-27 10:07:58 +0100

    Add TTL constant

--- clients/js/src/index.ts
+++ clients/js/src/index.ts
@@ -1,2 +1,12 @@
 export * from './generated';
 export * from './hooked';
+
+// The time-to-live for a slot value.
+//
+// If the slot is older than this value when compared to the current slot value, the
+// account derivation is considered invalid.
+//
+// This value is set to 150 slots, which is approximately 1 minute 19 seconds, assuming
+// 400ms block times. This is similar to the default value for the `recent_blockhash`
+// validation.
+export const TTL = 150;
--- clients/legacy/src/index.ts
+++ clients/legacy/src/index.ts
@@ -2,3 +2,13 @@ export * from './generated';
 export * from './hooked';
 export * from './pda';
 export * from './plugin';
+
+// The time-to-live for a slot value.
+//
+// If the slot is older than this value when compared to the current slot value, the
+// account derivation is considered invalid.
+//
+// This value is set to 150 slots, which is approximately 1 minute 19 seconds, assuming
+// 400ms block times. This is similar to the default value for the `recent_blockhash`
+// validation.
+export const TTL = 150;
