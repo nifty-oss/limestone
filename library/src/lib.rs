2fc0a6f79468798da65fd1d2196bb286b8385cac
febo <febo@nifty-oss.org>
2024-09-03 16:37:28 +0100

    Improve seed usage

--- library/src/lib.rs
+++ library/src/lib.rs
@@ -9,11 +9,13 @@ use solana_program::{
 /// invalid.
 ///
 /// This value is set to 150 slots, which is approximately 1 minute 19 seconds, assuming
-/// 400ms block times. This is similar to the default value for the `recent_blockhash`
-/// validation.
+/// 400ms block times. The value is the same as the [`MAX_PROCESSING_AGE`] - the maximum
+/// age of a blockhash that will be accepted by the leader.
+///
+/// [MAX_PROCESSING_AGE]: https://github.com/anza-xyz/agave/blob/master/sdk/program/src/clock.rs#L124-L127
 pub const DEFAULT_TTL: u64 = 150;
 
-/// The arguments for the `create_account` function.
+/// Main arguments for `create_account` functions.
 pub struct Arguments<'a, 'b> {
     /// Funding account.
     ///
@@ -26,17 +28,6 @@ pub struct Arguments<'a, 'b> {
     /// This account must be writable and signer.
     pub from: &'b AccountInfo<'a>,
 
-    /// Seeds for the account address derivation.
-    ///
-    /// If `None`, the address of the `from` account will be used. The `seeds` should not
-    /// include the `slot` value, since its value will be added to the seeds.
-    pub seeds: Option<&'b [&'b [u8]]>,
-
-    /// Slot to derive the new account.
-    ///
-    /// The slot must be within the TTL range.
-    pub slot: u64,
-
     /// Number of lamports to transfer to the new account.
     pub lamports: u64,
 
@@ -47,26 +38,25 @@ pub struct Arguments<'a, 'b> {
     ///
     /// If `None`, the program referenced by `program_id` will be used as owner.
     pub owner: Option<Pubkey>,
+
+    /// Slot to derive the new account.
+    ///
+    /// The slot must be within the TTL range.
+    pub slot: u64,
 }
 
-/// Creates a new account with the default TTL.
+/// Creates a new account with the standard `(from, slot)` derivation and default TTL.
 ///
-/// This function is a wrapper around [`create_account_with_ttl`] with the default TTL
-/// value.
-pub fn create_account(program_id: &Pubkey, arguments: Arguments) -> ProgramResult {
-    create_account_with_ttl(program_id, arguments, DEFAULT_TTL)
-}
-
-/// Creates a new account.
-///
-/// The address of the new account is derived from the `base` account and the `slot`
-/// value for the specifued `program_id`.
+/// The address of the new account is derived using the `from` account and the `slot`
+/// value for the specified `program_id`.
 ///
 /// The `slot` value must be within the `ttl` (time-to-live) range (`slot <= current slot + ttl`);
 /// otherwise, the account derivation is considered invalid and a `ProgramError::InvalidArgument`
 /// is returned.
 ///
-/// If the `base` is `None`, the address of the `from` account will be used in the derivation.
+/// # Required signer
+///
+/// The `from` account must be a signer.
 ///
 /// # Errors
 ///
@@ -74,10 +64,102 @@ pub fn create_account(program_id: &Pubkey, arguments: Arguments) -> ProgramResul
 ///
 /// - `ProgramError::InvalidArgument` if the slot is too old
 /// - `ProgramError::InvalidSeeds` if the derived address does not match the `to` account
-///   (`to` must match `[base, slot]`).
+///   (`to` must match `[from, slot]`).
+pub fn create_account(program_id: &Pubkey, arguments: Arguments) -> ProgramResult {
+    _create_account(program_id, arguments, None, DEFAULT_TTL)
+}
+
+/// Creates a new account with using the specified seeds and default TTL.
+///
+/// The address of the new account is derived using the `from` account, `seeds` and the `slot`
+/// value for the specified `program_id`.
+///
+/// The `slot` value must be within the `ttl` (time-to-live) range (`slot <= current slot + ttl`);
+/// otherwise, the account derivation is considered invalid and a `ProgramError::InvalidArgument`
+/// is returned.
+///
+/// # Required signer
+///
+/// The `from` account must be a signer.
+///
+/// # Errors
+///
+/// This function returns two errors:
+///
+/// - `ProgramError::InvalidArgument` if the slot is too old
+/// - `ProgramError::InvalidSeeds` if the derived address does not match the `to` account
+///   (`to` must match `[from, seeds, slot]`).
+pub fn create_account_with_seeds(
+    program_id: &Pubkey,
+    arguments: Arguments,
+    seeds: &[&[u8]],
+) -> ProgramResult {
+    _create_account(program_id, arguments, Some(seeds), DEFAULT_TTL)
+}
+
+/// Creates a new account with the standard `(from, slot)` derivation and the
+/// specified TTL.
+///
+/// The address of the new account is derived using the `from` account and the `slot`
+/// value for the specified `program_id`.
+///
+/// The `slot` value must be within the `ttl` (time-to-live) range (`slot <= current slot + ttl`);
+/// otherwise, the account derivation is considered invalid and a `ProgramError::InvalidArgument`
+/// is returned.
+///
+/// # Required signer
+///
+/// The `from` account must be a signer.
+///
+/// # Errors
+///
+/// This function returns two errors:
+///
+/// - `ProgramError::InvalidArgument` if the slot is too old
+/// - `ProgramError::InvalidSeeds` if the derived address does not match the `to` account
+///   (`to` must match `[from, slot]`).
 pub fn create_account_with_ttl(
     program_id: &Pubkey,
     arguments: Arguments,
+    ttl: u64,
+) -> ProgramResult {
+    _create_account(program_id, arguments, None, ttl)
+}
+
+/// Creates a new account with using the specified seeds and the specified TTL.
+///
+/// The address of the new account is derived using the `from` account, `seeds` and the `slot`
+/// value for the specified `program_id`.
+///
+/// The `slot` value must be within the `ttl` (time-to-live) range (`slot <= current slot + ttl`);
+/// otherwise, the account derivation is considered invalid and a `ProgramError::InvalidArgument`
+/// is returned.
+///
+/// # Required signer
+///
+/// The `from` account must be a signer.
+///
+/// # Errors
+///
+/// This function returns two errors:
+///
+/// - `ProgramError::InvalidArgument` if the slot is too old
+/// - `ProgramError::InvalidSeeds` if the derived address does not match the `to` account
+///   (`to` must match `[from, slot]`).
+pub fn create_account_with_seeds_and_ttl(
+    program_id: &Pubkey,
+    arguments: Arguments,
+    seeds: &[&[u8]],
+    ttl: u64,
+) -> ProgramResult {
+    _create_account(program_id, arguments, Some(seeds), ttl)
+}
+
+#[inline(always)]
+fn _create_account(
+    program_id: &Pubkey,
+    arguments: Arguments,
+    seeds: Option<&[&[u8]]>,
     ttl: u64,
 ) -> ProgramResult {
     // Check if the slot is too old.
@@ -91,8 +173,9 @@ pub fn create_account_with_ttl(
 
     let slot_seeds = arguments.slot.to_le_bytes();
     // Seeds has an extra allocation to store the bump value for the signer.
-    let mut seeds = if let Some(seeds) = arguments.seeds {
-        let mut seeds_with_slot = Vec::with_capacity(seeds.len() + 2);
+    let mut seeds = if let Some(seeds) = seeds {
+        let mut seeds_with_slot = Vec::with_capacity(seeds.len() + 3);
+        seeds_with_slot.push(arguments.from.key.as_ref());
         seeds_with_slot.extend_from_slice(seeds);
         seeds_with_slot.push(&slot_seeds);
         seeds_with_slot
