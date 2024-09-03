51d862b4de0a7fa5eefbd798edacb7636ec329b8
febo <febo@nifty-oss.org>
2024-09-03 16:39:22 +0100

    Update rust tests

--- clients/rust/tests/create_account.rs
+++ clients/rust/tests/create_account.rs
@@ -1,11 +1,12 @@
 #![cfg(feature = "test-sbf")]
 
-use limestone_client::{find_pda, instructions::CreateAccountBuilder};
+use limestone_client::{find_pda, find_pda_with_seed, instructions::CreateAccountBuilder};
 use solana_program_test::{tokio, ProgramTest};
 use solana_sdk::{
     clock::Clock,
     instruction::InstructionError,
     program_pack::IsInitialized,
+    pubkey::Pubkey,
     signature::{Keypair, Signer},
     system_program,
     transaction::Transaction,
@@ -36,19 +37,17 @@ async fn success_create_account() {
 
     // Given a PDA derived from a base pubkey and the current slot.
 
-    let base = Keypair::new();
     let slot = context
         .banks_client
         .get_sysvar::<Clock>()
         .await
         .unwrap()
         .slot;
-    let (pda, _) = find_pda(&base.pubkey(), slot);
+    let (pda, _) = find_pda(&context.payer.pubkey(), slot);
 
     // When we create an account for the PDA.
 
     let ix = CreateAccountBuilder::new()
-        .base(Some(base.pubkey()))
         .from(context.payer.pubkey())
         .to(pda)
         .slot(slot)
@@ -60,7 +59,56 @@ async fn success_create_account() {
     let tx = Transaction::new_signed_with_payer(
         &[ix],
         Some(&context.payer.pubkey()),
-        &[&context.payer, &base],
+        &[&context.payer],
+        context.last_blockhash,
+    );
+    context.banks_client.process_transaction(tx).await.unwrap();
+
+    // Then an account was created with the correct data.
+
+    let account = context.banks_client.get_account(pda).await.unwrap();
+
+    assert!(account.is_some());
+
+    let account = account.unwrap();
+    assert_eq!(account.lamports, 5_000_000_000);
+    assert_eq!(account.data.len(), 200);
+    assert_eq!(account.owner, system_program::ID);
+}
+
+#[tokio::test]
+async fn success_create_account_with_seed() {
+    let mut context = ProgramTest::new("limestone_program", limestone_client::ID, None)
+        .start_with_context()
+        .await;
+
+    // Given a PDA derived from a base pubkey and the current slot.
+
+    let seed = Pubkey::new_unique();
+    let slot = context
+        .banks_client
+        .get_sysvar::<Clock>()
+        .await
+        .unwrap()
+        .slot;
+    let (pda, _) = find_pda_with_seed(&context.payer.pubkey(), &seed.to_bytes(), slot);
+
+    // When we create an account for the PDA.
+
+    let ix = CreateAccountBuilder::new()
+        .from(context.payer.pubkey())
+        .to(pda)
+        .seed(Some(seed))
+        .slot(slot)
+        .lamports(5_000_000_000)
+        .space(200)
+        .owner(system_program::ID)
+        .instruction();
+
+    let tx = Transaction::new_signed_with_payer(
+        &[ix],
+        Some(&context.payer.pubkey()),
+        &[&context.payer],
         context.last_blockhash,
     );
     context.banks_client.process_transaction(tx).await.unwrap();
@@ -85,14 +133,13 @@ async fn fail_create_account_with_old_slot() {
 
     // Given a PDA derived from a base pubkey and the current slot.
 
-    let base = Keypair::new();
     let slot = context
         .banks_client
         .get_sysvar::<Clock>()
         .await
         .unwrap()
         .slot;
-    let (pda, _) = find_pda(&base.pubkey(), slot);
+    let (pda, _) = find_pda(&context.payer.pubkey(), slot);
 
     // And the slot is then incremented.
 
@@ -101,7 +148,6 @@ async fn fail_create_account_with_old_slot() {
     // When we try to create an account for the PDA with an old slot.
 
     let ix = CreateAccountBuilder::new()
-        .base(Some(base.pubkey()))
         .from(context.payer.pubkey())
         .to(pda)
         .slot(slot)
@@ -113,7 +159,7 @@ async fn fail_create_account_with_old_slot() {
     let tx = Transaction::new_signed_with_payer(
         &[ix],
         Some(&context.payer.pubkey()),
-        &[&context.payer, &base],
+        &[&context.payer],
         context.last_blockhash,
     );
     let err = context
@@ -135,7 +181,6 @@ async fn fail_create_account_with_invalid_derivation() {
 
     // Given a PDA derived from a base pubkey and the current slot.
 
-    let base = Keypair::new();
     let slot = context
         .banks_client
         .get_sysvar::<Clock>()
@@ -145,12 +190,11 @@ async fn fail_create_account_with_invalid_derivation() {
 
     // And we derive a PDA with a different slot.
 
-    let (pda, _) = find_pda(&base.pubkey(), slot + 100);
+    let (pda, _) = find_pda(&context.payer.pubkey(), slot + 100);
 
     // When we try to create an account for the PDA.
 
     let ix = CreateAccountBuilder::new()
-        .base(Some(base.pubkey()))
         .from(context.payer.pubkey())
         .to(pda)
         .slot(slot)
@@ -162,7 +206,7 @@ async fn fail_create_account_with_invalid_derivation() {
     let tx = Transaction::new_signed_with_payer(
         &[ix],
         Some(&context.payer.pubkey()),
-        &[&context.payer, &base],
+        &[&context.payer],
         context.last_blockhash,
     );
     let err = context
@@ -191,19 +235,18 @@ async fn success_create_and_initialize_account() {
         .await
         .unwrap()
         .slot;
-    let (pda, _) = find_pda(&authority.pubkey(), slot);
+    let (pda, _) = find_pda(&context.payer.pubkey(), slot);
 
     let account_size = ExtensionType::try_calculate_account_len::<Mint>(&[]).unwrap();
     let rent = context.banks_client.get_rent().await.unwrap();
 
     let create_ix = CreateAccountBuilder::new()
-        .base(Some(authority.pubkey()))
         .from(context.payer.pubkey())
         .to(pda)
-        .slot(slot)
         .lamports(rent.minimum_balance(account_size))
         .space(account_size as u64)
         .owner(spl_token_2022::ID)
+        .slot(slot)
         .instruction();
 
     let instructions = vec![
@@ -221,7 +264,7 @@ async fn success_create_and_initialize_account() {
     let tx = Transaction::new_signed_with_payer(
         &instructions,
         Some(&context.payer.pubkey()),
-        &[&context.payer, &authority],
+        &[&context.payer],
         context.last_blockhash,
     );
     context.banks_client.process_transaction(tx).await.unwrap();
