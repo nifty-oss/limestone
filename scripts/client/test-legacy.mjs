19cef213063bac5374159ba41fab2fb59f159b43
febo <febo@nifty-oss.org>
2024-10-14 09:53:04 +0100

    [wip] Add legacy package to workflow

---
+++ scripts/client/test-legacy.mjs
@@ -0,0 +1,12 @@
+#!/usr/bin/env zx
+import 'zx/globals';
+import { cliArguments, workingDirectory } from '../utils.mjs';
+
+// Start the local validator, or restart it if it is already running.
+await $`pnpm validator:restart`;
+
+// Build the client and run the tests.
+cd(path.join(workingDirectory, 'clients', 'legacy'));
+await $`pnpm install`;
+await $`pnpm build`;
+await $`pnpm test ${cliArguments()}`;
